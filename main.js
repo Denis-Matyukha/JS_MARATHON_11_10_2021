a03e005924238d5f7da3c0bccb955b53d1dfaff1
Denis Matyukha <denmatuhawork@gmail.com>
2021-10-14 13:11:25 +0300

    players1 and players2 are created

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log('Marathon....');
+// task_0
+const player1 = {
+    name: 'Scorpion',
+    hp: 100,
+    img: '',
+    weapon: ['keyboard'],
+    attack: function() {
+        console.log(`${this.name} Fight...`);
+    }
+};
+const player2 = {
+    name: 'Subzero',
+    hp: 95,
+    img: '',
+    weapon: ['keyboard'],
+    attack: function() {
+        console.log(`${this.name} Fight...`);
+    }
+};
