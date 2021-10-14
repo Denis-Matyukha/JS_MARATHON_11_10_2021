09b4242c53231465e5a8d80d8092b15e5f028803
Denis Matyukha <denmatuhawork@gmail.com>
2021-10-14 13:40:13 +0300

    fn createPlayer without args

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// task_0
 const player1 = {
     name: 'Scorpion',
     hp: 100,
@@ -8,6 +7,7 @@ const player1 = {
         console.log(`${this.name} Fight...`);
     }
 };
+
 const player2 = {
     name: 'Subzero',
     hp: 95,
@@ -17,3 +17,32 @@ const player2 = {
         console.log(`${this.name} Fight...`);
     }
 };
+
+const createPlayer = function() {
+    let $player = document.createElement('div');
+    $player.classList.add('player1');
+    
+    let $progressBar = document.createElement('div');
+    let $life = document.createElement('div');
+    let $name = document.createElement('div');
+    $progressBar.classList.add('progressbar');
+    $life.classList.add('life');
+    $name.classList.add('name');
+    // append ↓
+    $progressBar.append($life,$name);
+
+    
+    let $character = document.createElement('div');
+    let $img = document.createElement('img');
+    $img.src = 'http://reactmarathon-api.herokuapp.com/assets/liukang.gif';
+    $character.classList.add('character');
+    $character.appendChild($img);
+}
+
+/*
+- http://reactmarathon-api.herokuapp.com/assets/scorpion.gif
+- http://reactmarathon-api.herokuapp.com/assets/kitana.gif
+- http://reactmarathon-api.herokuapp.com/assets/liukang.gif
+- http://reactmarathon-api.herokuapp.com/assets/sonya.gif
+- http://reactmarathon-api.herokuapp.com/assets/subzero.gif
+*/
